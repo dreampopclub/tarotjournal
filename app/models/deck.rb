3373e2c2153d81944a831d6cb40bf6e8326f473e
Andrea Parthemore <andrea.parthemore@gmail.com>
2020-09-30 13:57:18 -0400

    Build a deck with suit and numbers for minor arcana.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,10 +1,45 @@
 class Deck
  attr_reader :cards
  def initialize
-   @cards = [
-     Card.new(:wands, 0),
-     Card.new(:wands, 1)
-   ]
+   @cards = build_deck
+ end
+
+ private
+
+ def build_deck
+   make_wands + make_cups + make_swords + make_pentacles
+ end
+
+ def make_wands
+   wands = []
+   for i in 1..14
+     wands << Card.new(:wands, i)
+   end
+   wands
+ end
+
+ def make_cups
+   cups = []
+   for i in 1..14
+     cups << Card.new(:cups, i)
+   end
+   cups
+ end
+
+ def make_swords
+   swords = []
+   for i in 1..14
+     swords << Card.new(:swords, i)
+   end
+   swords
+ end
+
+ def make_pentacles
+   pentacles = []
+   for i in 1..14
+     pentacles << Card.new(:pentacles, i)
+   end
+   pentacles
  end
 
 end
--- test/models/deck_test.rb
+++ test/models/deck_test.rb
@@ -2,7 +2,17 @@ require 'test_helper'
 
 class DeckTest < ActiveSupport::TestCase
   test "the Deck object is instantiated properly" do
+    # spot check the deck (minor arcana)
+    # suits are ordered wands, cups, swords, pentacles each in numerical order.
     deck = Deck.new
     assert_instance_of Card, deck.cards[0]
+    assert_equal 1, deck.cards[0].number
+    assert_equal :wands, deck.cards[0].suit
+    assert_equal 1, deck.cards[14].number
+    assert_equal :cups, deck.cards[14].suit
+    assert_equal 1, deck.cards[28].number
+    assert_equal :swords, deck.cards[28].suit
+    assert_equal 1, deck.cards[42].number
+    assert_equal :pentacles, deck.cards[42].suit
   end
 end
